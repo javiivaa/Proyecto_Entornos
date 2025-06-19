c3dde60b1552f193dcc5210c968556cd8352429f
javiivaa <javierxx49@gmail.com>
2025-06-19 17:37:15 +0200

    cambios realizados

--- src/acceso_datos/Acceso_datos.java
+++ src/acceso_datos/Acceso_datos.java
@@ -1,5 +1,42 @@
 package acceso_datos;
 
+import com.proyecto.entornos.modelo.Cliente;
+import com.proyecto.entornos.modelo.Producto;
+import com.proyecto.entornos.modelo.Pedido;
+
+/**
+ * Clase para métodos de acceso a la base de datos (lectura y escritura).
+ * Aquí se implementan los métodos CRUD para las entidades del sistema.
+ */
 public class Acceso_datos {
-	//Todos los metodos que impliquen la escritura  y lectura de base de datos
+    /**
+     * Inserta un cliente en la base de datos.
+     */
+    public void insertarCliente(Cliente cliente) {
+        // TODO: Implementar inserción en la base de datos
+    }
+
+    /**
+     * Obtiene un cliente por su ID.
+     */
+    public Cliente obtenerClientePorId(int id) {
+        // TODO: Implementar consulta a la base de datos
+        return null;
+    }
+
+    /**
+     * Actualiza los datos de un cliente.
+     */
+    public void actualizarCliente(Cliente cliente) {
+        // TODO: Implementar actualización en la base de datos
+    }
+
+    /**
+     * Elimina un cliente por su ID.
+     */
+    public void eliminarCliente(int id) {
+        // TODO: Implementar eliminación en la base de datos
+    }
+
+    // Métodos similares para Producto y Pedido...
 }
